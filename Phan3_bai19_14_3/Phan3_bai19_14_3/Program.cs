d1c3c0d69091e9d8a685385532ed76e7c0632212
vanngocthinhngo-lang <vanngocthinhngo@gmail.com>
2026-03-12 15:07:29 +0700

    NgocThinh_225LTC01_2415053122244

---
+++ Phan3_bai19_14_3/Phan3_bai19_14_3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> numbers = new List<int> { 1, 2, 2, 3, 3, 3, 4, 4, 5 };
+
+        var result = numbers
+                     .GroupBy(x => x)
+                     .OrderByDescending(g => g.Count())
+                     .First();
+
+        Console.WriteLine("So xuat hien nhieu nhat: " + result.Key);
+    }
+}
